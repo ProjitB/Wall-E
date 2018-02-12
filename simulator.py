0b84da97384c99bd6b5a38205e548f03e65cf3e8
AlokDebnath <AlokDebnath@github.com>
2018-02-12 23:53:56 +0530

    README added

--- simulator.py
+++ simulator.py
@@ -28,7 +28,7 @@ class Manual_Player:
 	def __init__(self):
 		pass
 	def move(self, board, old_move, flag):
-		print 'Enter your move: <format:row column> (you\'re playing with', flag + ")"	
+		print 'Enter your move: <format:row column> (you\'re playing with', flag + ')'	
 		mvp = raw_input()
 		mvp = mvp.split()
 		return (int(mvp[0]), int(mvp[1]))
